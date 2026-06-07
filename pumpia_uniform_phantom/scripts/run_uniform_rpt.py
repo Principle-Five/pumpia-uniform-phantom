82680492d2c588a6c847415a95dabf4a4dffd834
Zack <150851679+zravetz@users.noreply.github.com>
2026-06-07 21:31:16 +0100

    Changes to be committed: 	new file:   dicoms.nuitka-package.config.yml 	modified:   pumpia_uniform_phantom/scripts/run_uniform_rpt.py

--- pumpia_uniform_phantom/scripts/run_uniform_rpt.py
+++ pumpia_uniform_phantom/scripts/run_uniform_rpt.py
@@ -1,3 +1,14 @@
+# command line "python -m nuitka .\pumpia_uniform_phantom\scripts\run_uniform_rpt.py"
+
+# tk-inter is needed for tkinter to work with nuitka
+# nuitka documentation suggests it is not explicitly needed/found automatically but this is wrong
+# (possibly due to inclusion of matplotlib)
+
+# nuitka-project: --enable-plugin=tk-inter
+# nuitka-project: --mode=onefile
+# nuitka-project: --windows-console-mode=disable
+# nuitka-project: --user-package-configuration-file=dicoms.nuitka-package.config.yml
+
 from pumpia_uniform_phantom.repeat.collection import RepeatImagesCollection
 
 
